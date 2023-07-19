2d2bb6987ea75399e3dde40f5a85faf6e7b00e62
M Rizwan Uddin <125825286+MRizwanRohan@users.noreply.github.com>
2023-07-19 09:57:30 +0600

    Add files via upload
    Simple Input from user.

---
+++ Input from user.py
@@ -0,0 +1,11 @@
+name=input("Enter your name :")
+address=input("Enter your address =")
+phone=input("Input your phone number =")
+
+print("")
+print('//////////////////////')
+print("")
+
+print("Your name is",name)
+print("Your address is",address)
+print("Your phone number is "+phone)
