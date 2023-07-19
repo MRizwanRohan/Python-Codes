eecf6add715e5cb04bcc00bcf674f9c590f54212
M Rizwan Uddin <125825286+MRizwanRohan@users.noreply.github.com>
2023-07-19 09:44:01 +0600

    Add files via upload

---
+++ Python Multiply.py
@@ -0,0 +1,9 @@
+for x in range(1,2):
+    for y in range(1,11):
+        print(f"{x}*{y} =",x*y)
+
+print("##########")
+
+for x in range(2,3):
+    for y in range(1,11):
+        print(f"{x}*{y} =",x*y)
---
+++ Simple Sum.py
@@ -0,0 +1,6 @@
+x=1
+y=9
+
+sum=x+y
+
+print(sum)
